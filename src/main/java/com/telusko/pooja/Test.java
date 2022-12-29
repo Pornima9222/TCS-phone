6308e777b245c333c8118fd2bf1c57ab84d262ce
Pornima9222 <Pornimasakhare.ps@gmail.com>
2022-12-30 00:07:31 +0530

    Update

--- src/main/java/com/telusko/pooja/Test.java
+++ src/main/java/com/telusko/pooja/Test.java
@@ -1,7 +1,8 @@
 package com.telusko.pooja;
 
 public class Test {
-private int id = 101;
-private String name = "Alien";
-private int b = 20;
+	private int id = 101;
+	private String name = "Alien";
+	private int b = 20;
+	private int a = 30;
 }
